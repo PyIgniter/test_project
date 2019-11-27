2f35bec9e94d2e7869efc45c68693fd602253a6f
Andrii <empl.dmytrenko@gmail.com>
2019-11-27 12:29:39 +0200

    Add - number of DB queries

--- app1/views.py
+++ app1/views.py
@@ -9,7 +9,8 @@ class ReportOrder(View):
     """docstring for ReportOrder"""
     def get(self, request):
         order = Order.objects.all()
-        return render(request, 'app1/report_order.html', context={'order': order})
+        order_requests = order.count() 
+        return render(request, 'app1/report_order.html', context={'order': order, 'order_requests': order_requests})
 
 
 class ReportOrderItem(View):
@@ -17,16 +18,22 @@ class ReportOrderItem(View):
     def get(self, request):
         # колонки (имя товара, данные (Заказ {номер заказа} - Цена {цена товара в заказе} - Дата {дата покупки})
         data = []
-        for i in OrderItem.objects.values('product_name').distinct():
+        order_requests = 0
+        order_i = OrderItem.objects.values('product_name').distinct()
+        order_requests += order_i.count()
+        for i in order_i:
             list_order = []
-            for x in OrderItem.objects.filter(product_name=i['product_name']):
+            order_i_f = OrderItem.objects.filter(product_name=i['product_name'])
+            order_requests += order_i_f.count()
+            for x in order_i_f:
                 t = "Заказ {} - Цена {} - Дата {}".format(str(x.order), str(x.product_price), str(x.order.created_date.strftime("%d.%m.%Y %H:%M"))) 
                 list_order.append(t)
             list_order = str(list_order).strip("[]")
             i.update(data_order = list_order)
             data.append(i)
+
         # Самый покупаймый товар
         amount = OrderItem.objects.aggregate(Max('amount'))
 
         
-        return render(request, 'app1/report_order_item.html', context={'data': data, 'amount': amount})
+        return render(request, 'app1/report_order_item.html', context={'data': data, 'amount': amount, 'order_requests': order_requests})
